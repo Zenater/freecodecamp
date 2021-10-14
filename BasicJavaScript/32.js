32236e4f03e25c32e4301fe317d3a6c087adc8d7
Zenater <zent230096@mail.ru>
2021-10-14 16:20:56 +0300

    new problem

---
+++ BasicJavaScript/32.js
@@ -0,0 +1,17 @@
+/*wordBlanks should be a string.
+You should not change the values assigned to myNoun, myVerb, myAdjective or myAdverb.
+You should not directly use the values dog, ran, big, or quickly to create wordBlanks.
+wordBlanks should contain all of the words assigned to the variables myNoun, myVerb, myAdjective and myAdverb separated by non-word characters (and any additional words in your madlib).*/
+
+
+
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "My " + myNoun + " Molly is really " + myAdjective + " and it " + myVerb + " really " + myAdverb;
+ // Change this line
+// Only change code above this line
---
+++ BasicJavaScript/33.js
@@ -0,0 +1,9 @@
+/*myArray should be an array.
+
+The first item in myArray should be a string.
+
+madlib).*/
+
+
+
+var myArray = ["pioner", 4, "home"];

ac9aefd6e87e28277e9b0798ebfdec7400f562c3
Zenater <zent230096@mail.ru>
2021-10-15 17:07:22 +0300

    new p

---
+++ BasicJavaScript/35.js
@@ -0,0 +1,12 @@
+/*Example
+
+var array = [50,60,70];
+array[0];
+var data = array[1];
+array[0] is now 50, and data has the value 60.
+
+Note: There shouldn't be any spaces between the array name and the square brackets, like array [0]. Although JavaScript is able to process this correctly, this may confuse other programmers reading your code.
+
+Create a variable called myData and set it to equal the first value of myArray using bracket notation.*/
+var myArray = [50,60,70];
+var myData = myArray[0];

7398626aaa73d2551d103f6ed6f7ce1fbe8a7032
Zenater <zent230096@mail.ru>
2021-10-16 22:44:56 +0300

    new

---
+++ BasicJavaScript/39.js
@@ -0,0 +1,10 @@
+/*Use the .pop() function to remove the last item from myArray, assigning the popped off value to removedFromMyArray.
+myArray should only contain [["John", 23]].
+
+You should use pop() on myArray.
+
+removedFromMyArray should only contain ["cat", 2].*/
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
---
+++ BasicJavaScript/40.js
@@ -0,0 +1,9 @@
+/*Use the .shift() function to remove the first item from myArray, assigning the "shifted off" value to removedFromMyArray.
+myArray should now equal [["dog", 3]].
+
+removedFromMyArray should contain ["John", 23].*/
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();

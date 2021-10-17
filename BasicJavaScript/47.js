b4ab62fa9eaefd9e8db4c6ed3812ca9df8ccba23
Zenater <zent230096@mail.ru>
2021-10-17 21:55:34 +0300

    new

---
+++ BasicJavaScript/47.js
@@ -0,0 +1,22 @@
+/*TAdd a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+You should not change the value of the global outerWear.
+
+myOutfit should return the string sweater.
+
+Passed
+You should not change the return statement.
+*/
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+    

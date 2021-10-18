0ca4f169a75faa240f1c4bbce71b19ccaf4957b4
Zenater <zent230096@mail.ru>
2021-10-18 22:02:31 +0300

    new

---
+++ BasicJavaScript/50.js
@@ -0,0 +1,15 @@
+/*Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+processed should have a value of 2
+
+Passed
+You should assign processArg to processed.*/
+// Setup
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);

5fd9d13a7c7e6bf934287c92c716bcaa5b814350
Zenater <zent230096@mail.ru>
2021-10-18 22:01:18 +0300

    new task

---
+++ BasicJavaScript/48.js
@@ -0,0 +1,15 @@
+/*TCreate a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+timesFive should be a function
+
+Passed
+timesFive(5) should return 25
+
+Passed
+timesFive(2) should return 10
+
+ */
+function timesFive(num) {
+  return num * 5;
+}
+var answer = timesFive(5);
+    

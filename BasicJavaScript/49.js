012ebda0523c33fc63b3124c81002941427b6ff4
Zenater <zent230096@mail.ru>
2021-10-18 22:01:57 +0300

    new

---
+++ BasicJavaScript/49.js
@@ -0,0 +1,26 @@
+/*Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+Passed
+Once both functions have run, the sum should be equal to 8.
+
+Passed
+Returned value from addFive should be undefined.
+
+Passed
+Inside the addFive function, you should add 5 to the sum variable.*/
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();

9f915a9392f3ad2c710b9355c4c213fcf25ca8ef
Zenater <zent230096@mail.ru>
2021-10-19 13:53:23 +0300

    new

---
+++ BasicJavaScript/52.js
@@ -0,0 +1,16 @@
+/* Understanding Boolean Values
+Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+The welcomeToBooleans() function should return a Boolean (true or false) value.
+
+Passed
+welcomeToBooleans() should return true*/
+
+function welcomeToBooleans() {
+
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }

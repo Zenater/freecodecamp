8dc10040c29d8fd522bf7226187e9a0bab7cfdff
Zenater <zent230096@mail.ru>
2021-10-19 13:55:15 +0300

    new

---
+++ BasicJavaScript/56.js
@@ -0,0 +1,19 @@
+/* The compareEquality function in the editor compares two values using the equality operator.
+ Modify the function so that it returns the string Equal only when the values are strictly equal.
+
+compareEquality(10, "10") should return the string Not Equal
+
+Passed
+compareEquality("20", 20) should return the string Not Equal
+
+Passed
+You should use the === operator*/
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+  compareEquality("20", 20);
